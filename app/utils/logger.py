343952f687674fef93705c7de0ddf5c7808ca6f4
Sumit <128029605+repo-sumit@users.noreply.github.com>
2025-05-11 23:22:50 +0530

    Delete app/utils/logger.py

--- app/utils/logger.py
+++
@@ -1,7 +0,0 @@
-import csv
-from datetime import datetime
-
-def log_conversation(user_input, response_text):
-    with open("data/logs.csv", mode="a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([datetime.now(), user_input, response_text])
