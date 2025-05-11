a998c7011881e13b19e85d046db38119b442300d
Sumit <128029605+repo-sumit@users.noreply.github.com>
2025-05-11 23:23:06 +0530

    Delete app/utils/stt.py

--- app/utils/stt.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-model = whisper.load_model("base")
-
-def transcribe_audio(audio_file):
-    audio_path = "./temp.wav"
-    audio_file.save(audio_path)
-    result = model.transcribe(audio_path)
-    return result["text"]
